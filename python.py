24dd222755235225998d3eed0a9d4641f15b67fd
ramshekhar10 <E22BCAU0124@Bennett.edu.in>
2023-01-31 15:18:43 +0530

    cmt_msg_03

--- python.py
+++ python.py
@@ -3,20 +3,20 @@ class shape:
         self.color = color
     def area(self):
         pass
-    class Rectangle(shape):
-        def __init__(self,color,length,width):
-            super().__init__(color)
-            self.length = length
-            self.width = width
-        def area(self):
-            return self.length*self.width
-    class Circle(shape):
-        def __init__(self,color,radius):
-            super().__init__(color)
-            self.radius = radius   
-        def area(self):
-            return 3.14*self.radius*self.radius
-    rect=Rectangle("red",10,20)
-    print("Rectangle area:",rect.area(),"color:",rect.color)
-    circle =Circle("blue",5)
-    print("Circle area:",circle.area(),"color:",circle.color)
+class Rectangle(shape):
+    def __init__(self,color,length,width):
+        super().__init__(color)
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length*self.width
+class Circle(shape):
+    def __init__(self,color,radius):
+        super().__init__(color)
+        self.radius = radius   
+    def area(self):
+        return 3.14*self.radius*self.radius
+rect=Rectangle("red",10,20)
+print("Rectangle area:",rect.area(),"color:",rect.color)
+circle =Circle("blue",5)
+print("Circle area:",circle.area(),"color:",circle.color)
